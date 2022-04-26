160d70f9c5d9e238ad9f46d5e510cecc4d50e00a
townend22 <99081783+townend22@users.noreply.github.com>
2022-04-26 15:14:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from os import system
+m = "start"
+def main():
+    system("python3 main.py")
+
+while m!="stop":
+    print("")
+    print("System hacked")
+    print("")
+    main()
+    
